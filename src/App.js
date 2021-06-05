4d086b91f3abef8bfdf8b47c87256871ce5bdb4b
EugeniySichev <sichev91@inbox.ru>
2021-06-06 01:11:53 +0300

    add basic todo functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-
+import React from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './AddTodo'
 
 function App() {
-  return (<div className='wrapper'>
-    <h1>React tutorial</h1>
-  </div>)
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: "buy bread"},
+    {id: 2, completed: true, title: "buy huetu"},
+    {id: 3, completed: false, title: "raider debil"}
+  ])
+  
+
+  function toggleTodo(id){
+     setTodos(
+       todos.map(todo => {
+      if(todo.id == id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    )
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title){
+    setTodos(todos.concat([{
+      title, 
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+         <div className='wrapper'>
+          <h1>React tutorial</h1>
+          <AddTodo onCreate={addTodo}/>
+
+          {todos.length ? ( 
+          <TodoList todos={todos} onToggle={toggleTodo}/> 
+          ): ( 
+          <p>No todos!</p>
+          )}
+
+          
+        </div>
+    </Context.Provider>
+  
+  )
 }
 
 export default App;
+
